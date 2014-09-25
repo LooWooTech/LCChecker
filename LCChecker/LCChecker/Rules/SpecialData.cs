fff54205b2cc4d0f59d9d5b6be768043d0ad711b
wangjianlong <810844480@qq.com>
2014-09-25 15:43:15 +0800

    update

--- LCChecker/LCChecker/Rules/SpecialData.cs
+++ LCChecker/LCChecker/Rules/SpecialData.cs
@@ -10,6 +10,7 @@ namespace LCChecker.Rules
     public class SpecialData:IRowRule
     {
         public int ColumnIndex { get; set; }//检验的列
+        public int ColumnIndex2 { get; set; }
         public string Value { get; set; }
         public int IDIndex { get; set; }//项目编号所在的列
         public Dictionary<string, Index2> ProjectData { get; set; }
@@ -38,6 +39,7 @@ namespace LCChecker.Rules
                         return false;
                     break;
                 case "已与建设项目预挂钩应核销占补平衡指标":
+                    var cell2 = row.GetCell(ColumnIndex2 + xoffset, MissingCellPolicy.CREATE_NULL_AS_BLANK);
                     double val=.0;
                     if (cell.CellType == CellType.Numeric || cell.CellType == CellType.Formula)
                     {
@@ -45,11 +47,33 @@ namespace LCChecker.Rules
                         {
                             val = cell.NumericCellValue;
                         }
-                        catch {
+                        catch
+                        {
                             val = .0;
                         }
                     }
-                    if (Math.Abs(val - item.Indicators) > 0.0001)
+                    else {
+                        var value2 = cell.ToString().Trim();
+                        double.TryParse(value2, out val);
+                    }
+                    double val2;
+                    if (cell2.CellType == CellType.Numeric || cell2.CellType == CellType.Formula)
+                    {
+                        try
+                        {
+                            val2 = cell2.NumericCellValue;
+                        }
+                        catch
+                        {
+                            val2 = .0;
+                        }
+                    }
+                    else {
+                        var value2 = cell2.ToString().Trim();
+                        double.TryParse(value2, out val2);
+                    }
+                    double sum = val + val2;
+                    if (Math.Abs(sum - item.Indicators) > 0.0001)
                         return false;
                     break;
                 //case "新增耕地":
