df537528349bfbfe4c1a3356a33befa2bd4f457f
wangjianlong <810844480@qq.com>
2014-10-16 11:56:12 +0800

    update

--- LCChecker/LCChecker/Models/CheckReport4.cs
+++ LCChecker/LCChecker/Models/CheckReport4.cs
@@ -17,7 +17,7 @@ namespace LCChecker.Models
             }
             var list = new List<IRowRule>();
             list.Add(new OnlyProject() { ColumnIndex = 3, Projects=Team,Values = new[] { "项目编号", "市", "县"},ID="2401" });
-            list.Add(new CellRangeRowRule() { ColumnIndex = 4, Values = new[] { "重复备案项目", "增减挂钩项目误备案至农村土地整治监测监管系统", "经核实，项目由于___原因未实施或未终止实施，详细说明具体情况" },ID="2402" });
+            //list.Add(new CellRangeRowRule() { ColumnIndex = 4, Values = new[] { "重复备案项目", "增减挂钩项目误备案至农村土地整治监测监管系统", "经核实，项目由于___原因未实施或未终止实施，详细说明具体情况" },ID="2402" });
             list.Add(new CellRangeRowRule() { ColumnIndex = 5, Values = new[] { "是", "否" } ,ID="2403"});
             list.Add(new CellRangeRowRule() { ColumnIndex = 6, Values = new[] { "是", "否" } ,ID="2404"});
             
