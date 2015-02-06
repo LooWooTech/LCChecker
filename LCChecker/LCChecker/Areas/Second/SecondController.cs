9dd8b3c83c0effad8490e973ddde80e9bdede640
wangjianlong <810844480@qq.com>
2015-02-06 10:26:22 +0800

    commit

--- LCChecker/LCChecker/Areas/Second/SecondController.cs
+++ LCChecker/LCChecker/Areas/Second/SecondController.cs
@@ -1,9 +1,17 @@
 ﻿
 
+using System.Web;
 namespace LCChecker.Areas.Second
 {
+    [UserAuthorize]
     public class SecondController : LCChecker.Controllers.ControllerBase
     {
-
+        protected override void OnActionExecuting(System.Web.Mvc.ActionExecutingContext filterContext)
+        {
+            //if (!CurrentUser.Flag) {
+            //    throw new HttpException(401,"你没有");
+            //}
+            base.OnActionExecuting(filterContext);
+        }    
     }
 }
